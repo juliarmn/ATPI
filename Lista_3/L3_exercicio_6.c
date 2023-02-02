d3145edf61debfe11de7571f04c9baa338df081b
juliarmn <105869092+juliarmn@users.noreply.github.com>
2023-02-01 22:55:33 -0300

    Update L3_exercicio_6.c

--- Lista_3/L3_exercicio_6.c
+++ Lista_3/L3_exercicio_6.c
@@ -1,2 +1,18 @@
 /*Ler um número e imprimir se o mesmo é positivo, negativo ou nulo.*/
 
+#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
+  int num;
+  printf("Digite um número intero\n");
+  scanf ("%d", &num);
+  if (num == 0)
+    printf ("Esse número é nulo");
+  else if (num < 0)
+    printf ("O número %d é negativo", num);
+   else if (num > 0)
+    printf ("O número %d é positivo", num);
+  return 0;
+}
