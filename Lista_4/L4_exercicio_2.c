6fa857b3d02930116b6832511a286c01d5bdf51c
Júlia Rodrigues (Jua) <105869092+juliarmn@users.noreply.github.com>
2023-02-02 11:40:13 -0300

    Create L4_exercicio_2.c

---
+++ Lista_4/L4_exercicio_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) { //variáveis:
+  float KWH, preço;
+  int idade, residencias;
+  preço=0.1; //não é necessário colocar, o valor vai modificar sempre
+  printf ("\nDigite a quantidade de residências analisadas\n" );
+  scanf ("%d", &residencias);
+  for (int i = 1; i <= residencias; i ++) { 
+      printf ("\nDigite a quantidade de Kwh consumidas no mês na residência %d\n", i);
+      scanf ( " %f", &KWH);
+      printf ("\nDigite a idade do morador\n");
+      scanf ( " %d", &idade);
+      if (idade >= 70) {
+        preço= KWH*0.1*0.75;
+        printf ("\nO valor a ser pago na residência %d é: %.2f\n", i, preço);
+      } 
+      else {
+        preço=KWH*0.1;
+        printf ("\nO valor a ser pago na residência %d é: %.2f\n", i, preço);
+      }
+  }
+  return 0;
+}
